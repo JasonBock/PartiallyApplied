0d873e1d07de04b9973732a14087677ff3754c6e
Jason Bock <jason.r.bock@outlook.com>
2021-11-28 12:07:38 -0600

    #14 all cleanup is done

--- src/PartiallyApplied.Tests/TestAssistants.cs
+++ src/PartiallyApplied.Tests/TestAssistants.cs
@@ -28,30 +28,4 @@ internal static class TestAssistants
 		test.TestState.ExpectedDiagnostics.AddRange(expectedDiagnostics);
 		await test.RunAsync().ConfigureAwait(false);
 	}
-
-	//internal static async Task RunAsync<T>(string code,
-	//	IEnumerable<(Type, string, string)> generatedSources,
-	//	IEnumerable<DiagnosticResult> expectedDiagnostics,
-	//	OutputKind outputKind = OutputKind.DynamicallyLinkedLibrary)
-	//	where T : ISourceGenerator, new()
-	//{
-	//	var test = new CSharpSourceGeneratorTest<T, NUnitVerifier>
-	//	{
-	//		ReferenceAssemblies = ReferenceAssemblies.Net.Net50,
-	//		TestState =
-	//		{
-	//			Sources = { code },
-	//			OutputKind = outputKind
-	//		},
-	//	};
-
-	//	foreach (var generatedSource in generatedSources)
-	//	{
-	//		test.TestState.GeneratedSources.Add(generatedSource);
-	//	}
-
-	//	test.TestState.AdditionalReferences.Add(typeof(T).Assembly);
-	//	test.TestState.ExpectedDiagnostics.AddRange(expectedDiagnostics);
-	//	await test.RunAsync().ConfigureAwait(false);
-	//}
 }
--- src/PartiallyApplied/PartiallyAppliedGenerator.cs
+++ src/PartiallyApplied/PartiallyAppliedGenerator.cs
@@ -67,33 +67,3 @@ public sealed class PartiallyAppliedGenerator
 		}
 	}
 }
-
-//[Generator]
-//public sealed class PartiallyAppliedGenerator
-//	: ISourceGenerator
-//{
-//	public void Execute(GeneratorExecutionContext context)
-//	{
-//		if (context.SyntaxReceiver is PartiallyAppliedReceiver receiver)
-//		{
-//			var compilation = context.Compilation;
-//			context.CancellationToken.ThrowIfCancellationRequested();
-//			var information = new PartiallyAppliedInformation(receiver.Candidates.ToImmutableArray(), compilation);
-
-//			foreach (var diagnostic in information.Diagnostics)
-//			{
-//				context.ReportDiagnostic(diagnostic);
-//			}
-
-//			if (!information.Diagnostics.Any(_ => _.Severity == DiagnosticSeverity.Error) &&
-//				information.Results.Length > 0)
-//			{
-//				var builder = new PartiallyAppliedBuilder(information);
-//				context.AddSource(Shared.GeneratedFileName, builder.Code);
-//			}
-//		}
-//	}
-
-//	public void Initialize(GeneratorInitializationContext context) =>
-//		context.RegisterForSyntaxNotifications(() => new PartiallyAppliedReceiver());
-//}
