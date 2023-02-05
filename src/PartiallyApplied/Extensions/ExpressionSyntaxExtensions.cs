56cb790691a72a9659e88484ab2293e1c0601c4c
Blu3 <Blu3SoulsIT@gmail.com>
2023-02-05 03:50:21 +0100

    Implemented feature

--- src/PartiallyApplied/Extensions/ExpressionSyntaxExtensions.cs
+++ src/PartiallyApplied/Extensions/ExpressionSyntaxExtensions.cs
@@ -1,36 +1,40 @@
-﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System.Collections.Immutable;
 
 namespace PartiallyApplied.Extensions;
 
 internal static class ExpressionSyntaxExtensions
 {
-	internal static ImmutableArray<(IMethodSymbol?, bool)> TryGetMethodSymbols(this ExpressionSyntax self, SemanticModel model)
-	{
-		var symbols = new List<(IMethodSymbol?, bool)>();
+   internal static ImmutableArray<(IMethodSymbol?, bool)> TryGetMethodSymbols(this ExpressionSyntax self, SemanticModel model)
+   {
+	  var symbols = new List<(IMethodSymbol?, bool)>();
 
-		var symbol = model.GetSymbolInfo(self);
+	  var symbol = model.GetSymbolInfo(self);
 
-		if (symbol.Symbol is IMethodSymbol methodSymbol)
-		{
-			symbols.Add((methodSymbol, true));
-		}
-		else if (symbol.CandidateSymbols.Length > 0)
-		{
-			foreach (var candidate in symbol.CandidateSymbols)
+	  if (symbol.Symbol is IMethodSymbol methodSymbol)
+	  {
+		 symbols.Add((methodSymbol, true));
+	  }
+	  else if (model.GetTypeInfo(self).Type is INamedTypeSymbol { TypeKind: TypeKind.Delegate, DelegateInvokeMethod: { } delegateMethodSymbol })
+	  {
+		 symbols.Add((delegateMethodSymbol, false));
+	  }
+	  else if (symbol.CandidateSymbols.Length > 0)
+	  {
+		 foreach (var candidate in symbol.CandidateSymbols)
+		 {
+			if (candidate is IMethodSymbol methodFromCandidateSymbol)
 			{
-				if (candidate is IMethodSymbol methodFromCandidateSymbol)
-				{
-					symbols.Add((methodFromCandidateSymbol, false));
-				}
+			   symbols.Add((methodFromCandidateSymbol, false));
 			}
-		}
-		else
-		{
-			symbols.Add((null, false));
-		}
+		 }
+	  }
+	  else
+	  {
+		 symbols.Add((null, false));
+	  }
 
-		return symbols.ToImmutableArray();
-	}
+	  return symbols.ToImmutableArray();
+   }
 }
